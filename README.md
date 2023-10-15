e7d23650e18d1dbbb7af52f7f7cf70d92d196345
HA3IK [Nazar Holubovskyy] <golubha3ik@gmail.com>
2023-10-15 15:46:15 +0100

    1st commit

---
+++ README.md
@@ -0,0 +1,88 @@
+# Frontend Mentor - Age calculator app solution
+
+This is a solution to the [Age calculator app challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/age-calculator-app-dF9DFFpj-Q).  
+Frontend Mentor challenges help you improve your coding skills by building realistic projects.  
+The solution *live URL*: [Age calculator app by HA3IK](https://ha3ik.github.io/fep/age-calculator-app)
+
+## Table of contents
+
+- [Overview](#overview)
+- [About](#about)
+  - [Built with](#built-with)
+  - [Author](#author)
+
+## Overview
+
+### Submit event
+
+✅ Turned off event triggering on the button icon via CSS
+```css
+ button > img {
+  pointer-events: none;
+ }
+```
+
+✅ Preventing revalidation if input fields have not changed
+```js
+// IF the input data has not changed THEN return
+let formData = new FormData(form).values();
+formData = [...formData].join(',');
+if (formDataPrev === formData) return;
+// ELSE save the form data string:
+formDataPrev = formData;
+```
+
+### Accessibility
+
+✅ Reading the output content:
+  - `[aria-atomic=true]` on the wrapper so that the entire response is always read: n years, n months, n days
+  - `[aria-live=assertive]` on the wrapper so that the result is read immediately only if the answer is successful, and turned `off` if an error.
+  - `[aria-live=off]` for the `OUTPUT` tag, which is enabled by default. Avoiding double notification.
+  - The `"- -"` placeholder is labeled `[aria-label=none]`. If some number, `[aria-label]` is removed.
+  - Error messages are `[role=alert]`, and `[hidden]` by default.
+
+### Form
+  - ✅ Used `INPUT[type=number]` with `[min]` and `[max]` attributes.  
+  Default arrows (the spinner) are hidden via CSS:
+```css
+/* Webkit and Blink browsers */
+input[type="number"]::-webkit-inner-spin-button,
+input[type="number"]::-webkit-outer-spin-button {
+  -webkit-appearance: none;
+}
+
+/* Firefox and others */
+input[type="number"] {
+  -moz-appearance: textfield;
+}
+```
+
+### Assets
+
+✅ Added svg arrow for the mobile version  
+&emsp;&emsp;\- The arrows for the mobile version and for the desktop version are different.
+
+![PC vs mobile arrow icon difference](_challenge/screenshot/arrow-diff.jpg)
+
+### HTML validator error ([still there](https://github.com/HA3IK/fep/tree/skilled-elearning-landing-page#html-validator-error-again))
+
+⚠️ "Nu Html Checker" still hasn't fix the bug to recognize the modern LINK tag structure, according to the official WHATWG Living Standard.
+
+### Lighthouse
+
+✅ See the [Lighthouse report (for mobile)](_challenge/lighthouse-report-mob.html) here.
+
+## About
+
+### Built with
+
+- HTML: Semantic and compliant with the WHATWG "Living Standard", WAI-ARIA…
+- CSS: Responsive, Flexbox, fallback font-faces…
+- JS: ES6+, ValidityState API…
+- [Sass](https://sass-lang.com/) - CSS preprocessor.
+- [TypeScript](https://www.typescriptlang.org/) - strongly typed JS.
+
+### Author
+
+- Frontend Mentor - [@HA3IK](https://www.frontendmentor.io/profile/ha3ik)
+- Twitter - [@HA3IK](https://www.twitter.com/ha3ik)
---
+++ js/_ts/main.ts
@@ -0,0 +1,273 @@
+'use strict';
+
+/**
+ * Declaration
+ */
+
+interface IInputCollection {
+  ['input-day']: HTMLInputElement;
+  ['input-month']: HTMLInputElement;
+  ['input-year']: HTMLInputElement;
+}
+
+interface IOutputCollection {
+  ['output-day']: HTMLOutputElement;
+  ['output-month']: HTMLOutputElement;
+  ['output-year']: HTMLOutputElement;
+}
+
+interface IFormCollection extends IInputCollection, IOutputCollection, HTMLFormControlsCollection {
+  ['date-field']: HTMLFieldSetElement;
+}
+
+interface IMsgErrorProp {
+  readonly invalid: string;
+  max: string;
+  min: string;
+  tag?: HTMLParagraphElement;
+}
+
+interface IMsgError {
+  date: IMsgErrorProp;
+  input: {
+    [key in keyof IInputCollection]: IMsgErrorProp;
+  };
+  readonly required: string;
+}
+
+/**
+ * Code
+ */
+
+// The form validation list
+const MSG_ERROR: IMsgError = {
+  date: {
+    invalid: 'Must be a valid date',
+    max: 'The date must be in the past or today',
+    min: '', // The value is generated below
+  },
+  input: {
+    'input-day': {
+      invalid: 'Must be a valid day',
+      max: '', // The value is generated below
+      min: 'Must be >= 1',
+    },
+    'input-month': {
+      invalid: 'Must be a valid month',
+      max: 'Must be <= 12',
+      min: 'Must be >= 1',
+    },
+    'input-year': {
+      invalid: 'Must be a valid year',
+      max: 'Must be in the past',
+      min: '', // The value is generated below
+    },
+  },
+  required: 'This field is required',
+};
+// Upd. according to the task design
+MSG_ERROR.input['input-day'].min = MSG_ERROR.input['input-day'].max = MSG_ERROR.input['input-day'].invalid;
+MSG_ERROR.input['input-month'].min = MSG_ERROR.input['input-month'].max = MSG_ERROR.input['input-month'].invalid;
+
+const btnSubmit = document.querySelector('.btn') as HTMLButtonElement;
+const inputArr = Object.keys(MSG_ERROR.input) as Array<keyof IInputCollection>;
+const outputArr: Array<keyof IOutputCollection> = ['output-day', 'output-month', 'output-year'];
+const outputData = document.body.querySelector('.output-row') as HTMLDivElement;
+// Previous error state
+const wasError = {
+  date: false,
+  input: false,
+};
+
+let inputDateMin: Date;
+let formDataPrev: string;
+
+// Click event handler
+btnSubmit.addEventListener('click', (e): void => {
+  // Stop submit event
+  e.preventDefault();
+  // Define the form element
+  const form = (e.target as HTMLInputElement).form as HTMLFormElement;
+
+  // IF the input data has not changed THEN return
+  let formData: IterableIterator<FormDataEntryValue> | string = new FormData(form).values();
+  formData = [...formData].join(',');
+  if (formDataPrev === formData) return;
+  // ELSE save the form data string:
+  formDataPrev = formData;
+
+  // Define error vars
+  let isError: boolean;
+  let msgError: string;
+  // Define the form elements
+  const formCollection = form.elements as IFormCollection;
+  // Define the current date
+  const dateNow = new Date();
+  dateNow.setHours(0, 0, 0, 0); // Reset time
+  const dateYear = dateNow.getFullYear();
+  // Upd. data related to the year
+  if (Number(formCollection['input-year'].max) !== dateYear) {
+    formCollection['input-year'].max = `${dateYear}`;
+  }
+  if (!MSG_ERROR.input['input-year'].min) {
+    MSG_ERROR.input['input-year'].min = `Must be >= ${formCollection['input-year'].min}`;
+  }
+
+  // (Commented, according to the task design)
+  // Checking and generating [max] day and the error message of the max day number of the month
+  // const inputMonthDayMax = new Date(Number(formCollection['input-year'].value), Number(formCollection['input-month'].value), 0).getDate();
+  // if (Number(formCollection['input-day'].max) !== inputMonthDayMax) formCollection['input-day'].max = `${inputMonthDayMax}`;
+  // if (!MSG_ERROR.input['input-day'].max) MSG_ERROR.input['input-day'].max = `Must be <= ${formCollection['input-day'].max}`;
+
+  /**
+   * 1. Check input fields
+   * - IF the form is invalid
+   * - Check all input fields
+   * - Show/hide error messages
+   * - IF the form is valid OR was invalid
+   * - Just hide error messages
+   */
+
+  const formValidity = !form.checkValidity(); // (Opposite state)
+
+  if (formValidity || wasError.input) {
+    // Save the error state
+    wasError.input = formValidity;
+    // Check all input fields
+    inputArr.forEach((inputName): void => {
+      isError = formValidity;
+      msgError = '';
+      const inputError = MSG_ERROR.input[inputName];
+      const inputValidity = formCollection[inputName].validity;
+      // Define the error message element
+      if (!inputError.tag) {
+        inputError.tag = formCollection[inputName].parentNode!.querySelector('.date-msg-error') as HTMLParagraphElement;
+      }
+      // IF error THEN check the error type
+      if (isError) {
+        if (inputValidity.valueMissing) {
+          msgError = MSG_ERROR.required;
+        } else if (inputValidity.rangeUnderflow) {
+          msgError = inputError.min;
+        } else if (inputValidity.rangeOverflow) {
+          msgError = inputError.max;
+        } else {
+          isError = false; // Unplanned error 😅
+        }
+      }
+      // IF an error THEN insert the error message
+      if (isError) inputError.tag.textContent = msgError;
+      // IF NOT an error THEN the error message is "hidden" (hide it)
+      inputError.tag.hidden = !isError;
+    });
+  }
+
+  /**
+   * 2. Check the full input date
+   * - Share variables with output (step 3)
+   * - Inputs are valid (step 1) OR full date was invalid
+   * - IF inputs are invalid
+   * - Undo full date "was" state AND hide the error message
+   * - IF inputs are valid
+   * - Check the full date
+   * - Show/hide the error message
+   */
+
+  let inputYear!: number, inputMonth!: number, inputDay!: number, inputDateMs!: number;
+
+  if (!wasError.input || wasError.date) {
+    msgError = '';
+    // Define the error message element
+    if (!MSG_ERROR.date.tag) {
+      MSG_ERROR.date.tag = formCollection['date-field'].querySelector(
+        ':scope > .date-msg-error',
+      ) as HTMLParagraphElement;
+    }
+    // IF there is an input error after the date error
+    if (wasError.input) {
+      isError = false;
+    } else {
+      isError = true;
+      // Define the input date values
+      inputDay = Number(formCollection['input-day'].value);
+      inputMonth = Number(formCollection['input-month'].value);
+      inputYear = Number(formCollection['input-year'].value);
+      const inputDate = new Date(inputYear, inputMonth - 1, inputDay);
+      inputDateMs = inputDate.getTime();
+      if (!inputDateMin) inputDateMin = new Date(Number(formCollection['input-year'].min), 0, 1); // (YYYY.min)-01-01
+      // Check the full date from inputs
+      if (inputDate.getDate() != inputDay) {
+        msgError = MSG_ERROR.date.invalid; // The day number of the month is mismatched
+      } else if (inputDateMs > dateNow.getTime()) {
+        msgError = MSG_ERROR.date.max;
+      } else if (inputDateMs < inputDateMin.getTime()) {
+        if (!MSG_ERROR.date.min) {
+          MSG_ERROR.date.min = `Must be after ${inputDateMin.toLocaleDateString(navigator.language)}`;
+        }
+        msgError = MSG_ERROR.date.min;
+      } else {
+        isError = false;
+      }
+    }
+    // IF an error THEN insert the error message
+    if (isError) MSG_ERROR.date.tag.textContent = msgError;
+    // IF NOT an error THEN the error message is false (hide it)
+    MSG_ERROR.date.tag.hidden = !isError;
+    // Save the error state
+    wasError.date = isError;
+  }
+
+  /**
+   * 3. Output the result
+   * - IF any error THEN show the placeholder
+   * - ELSE calculate the difference in years, months and days
+   */
+
+  const inputDateClassList = formCollection['date-field'].classList;
+
+  // IF there was any error THEN show the placeholder
+  if (wasError.input || wasError.date) {
+    // Add error class
+    inputDateClassList.add('date-error');
+    // Accessibility
+    if (outputData.ariaLive !== 'off') {
+      outputData.ariaLive = 'off';
+      outputArr.forEach((outputName): void => {
+        const outputTag = formCollection[outputName];
+        outputTag.ariaLabel = 'none';
+        outputTag.value = outputTag.defaultValue; // Add the default value (placeholder)
+      });
+    }
+  } else {
+    // Remove error class
+    inputDateClassList.remove('date-error');
+    // Accessibility
+    if (outputData.ariaLive === 'off') {
+      outputArr.forEach((outputName): void => {
+        formCollection[outputName].removeAttribute('aria-label'); // Remove [aria-label=none]
+      });
+      outputData.ariaLive = 'assertive';
+    }
+    // Get current date values
+    const dateDay = dateNow.getDate();
+    const dateMonth = dateNow.getMonth();
+    const dateMonthDayMax = new Date(dateYear, dateMonth, 0).getDate();
+    // Input date is less or equal to the date now of the same year
+    const leDateNow = new Date(dateNow).setFullYear(inputYear) >= inputDateMs;
+    // Input day is greater than day now
+    const gtDayNow = dateDay < inputDay;
+    // Calc. years
+    let diffInYears = dateYear - inputYear;
+    diffInYears += leDateNow ? 0 : -1;
+    formCollection['output-year'].value = `${diffInYears}`;
+    // Calc. months
+    let diffInMonths = dateMonth - inputMonth;
+    diffInMonths += leDateNow ? 0 : 12;
+    diffInMonths += gtDayNow ? 0 : 1;
+    formCollection['output-month'].value = `${diffInMonths}`;
+    // Calc. days
+    let diffInDays = dateDay - inputDay;
+    diffInDays += gtDayNow ? dateMonthDayMax : 0;
+    formCollection['output-day'].value = `${diffInDays}`;
+  }
+});
